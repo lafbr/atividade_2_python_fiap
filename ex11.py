1819ebbf55c6fa3a5668ff90dbc4739f0b966b36
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-16 22:36:08 -0300

    adição dos demais exercicios feitos em aula

--- ex11.py
+++ ex11.py
@@ -1,4 +1,12 @@
 import os
 os.system ("cls")
 
+sit = str(input("Como esta o sistema? "))
 
+if sit == "n":
+
+    print(" Pode acessar o sistema!!")
+
+else:
+
+    print("não pode acessar o sistema!!")
